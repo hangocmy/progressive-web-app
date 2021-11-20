ef63f24f44f095cb6ef816ea9dc11115940ff442
hangocmy <ngocmytp255@gmail.com>
2021-11-20 14:55:48 +0700

    Update
    Img, index.html, serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,44 +5,43 @@ var cacheName = 'cache-v4';
 
 //Files to save in cache
 var files = [
-  './',
-  './index.html?utm=homescreen', //SW treats query string as new request
-  'https://fonts.googleapis.com/css?family=Roboto:200,300,400,500,700', //caching 3rd party content
-  './css/styles.css',
-  './images/icons/android-chrome-192x192.png',
-  './images/push-on.png',
-  './images/push-off.png',
-  './images/icons/favicon-16x16.png',
-  './images/icons/favicon-32x32.png',
-  './js/main.js',
-  './js/app.js',
-  './js/offline.js',
-  './js/push.js',
-  './js/sync.js',
-  './js/toast.js',
-  './js/share.js',
-  './js/menu.js',
-  './manifest.json'
+    './',
+    './index.html?utm=homescreen', //SW treats query string as new request
+    'https://fonts.googleapis.com/css?family=Roboto:200,300,400,500,700', //caching 3rd party content
+    './css/styles.css',
+    './images/icons/android-chrome-192x192.png',
+    './images/push-on.png',
+    './images/push-off.png',
+    './images/icons/favicon-16x16.png',
+    './images/icons/favicon-32x32.png',
+    './js/main.js',
+    './js/app.js',
+    './js/offline.js',
+    './js/push.js',
+    './js/sync.js',
+    './js/toast.js',
+    './js/share.js',
+    './manifest.json'
 ];
 
 //Adding `install` event listener
 self.addEventListener('install', (event) => {
-  console.info('Event: Install');
+    console.info('Event: Install');
 
-  event.waitUntil(
-    caches.open(cacheName)
-    .then((cache) => {
-      //[] of files to cache & if any of the file not present `addAll` will fail
-      return cache.addAll(files)
-      .then(() => {
-        console.info('All files are cached');
-        return self.skipWaiting(); //To forces the waiting service worker to become the active service worker
-      })
-      .catch((error) =>  {
-        console.error('Failed to cache', error);
-      })
-    })
-  );
+    event.waitUntil(
+        caches.open(cacheName)
+        .then((cache) => {
+            //[] of files to cache & if any of the file not present `addAll` will fail
+            return cache.addAll(files)
+                .then(() => {
+                    console.info('All files are cached');
+                    return self.skipWaiting(); //To forces the waiting service worker to become the active service worker
+                })
+                .catch((error) => {
+                    console.error('Failed to cache', error);
+                })
+        })
+    );
 });
 
 /*
@@ -51,43 +50,43 @@ self.addEventListener('install', (event) => {
 
 //Adding `fetch` event listener
 self.addEventListener('fetch', (event) => {
-  console.info('Event: Fetch');
+    console.info('Event: Fetch');
 
-  var request = event.request;
-  var url = new URL(request.url);
-  if (url.origin === location.origin) {
-    // Static files cache
-    event.respondWith(cacheFirst(request));
-  } else {
-    // Dynamic API cache
-    event.respondWith(networkFirst(request));
-  }
+    var request = event.request;
+    var url = new URL(request.url);
+    if (url.origin === location.origin) {
+        // Static files cache
+        event.respondWith(cacheFirst(request));
+    } else {
+        // Dynamic API cache
+        event.respondWith(networkFirst(request));
+    }
 
-  // // Checking for navigation preload response
-  // if (event.preloadResponse) {
-  //   console.info('Using navigation preload');
-  //   return response;
-  // }
+    // // Checking for navigation preload response
+    // if (event.preloadResponse) {
+    //   console.info('Using navigation preload');
+    //   return response;
+    // }
 });
 
 async function cacheFirst(request) {
-  const cachedResponse = await caches.match(request);
-  return cachedResponse || fetch(request);
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || fetch(request);
 }
 
 async function networkFirst(request) {
-  const dynamicCache = await caches.open(cacheName);
-  try {
-    const networkResponse = await fetch(request);
-    // Cache the dynamic API response
-    dynamicCache.put(request, networkResponse.clone()).catch((err) => {
-      console.warn(request.url + ': ' + err.message);
-    });
-    return networkResponse;
-  } catch (err) {
-    const cachedResponse = await dynamicCache.match(request);
-    return cachedResponse;
-  }
+    const dynamicCache = await caches.open(cacheName);
+    try {
+        const networkResponse = await fetch(request);
+        // Cache the dynamic API response
+        dynamicCache.put(request, networkResponse.clone()).catch((err) => {
+            console.warn(request.url + ': ' + err.message);
+        });
+        return networkResponse;
+    } catch (err) {
+        const cachedResponse = await dynamicCache.match(request);
+        return cachedResponse;
+    }
 }
 
 /*
@@ -96,39 +95,39 @@ async function networkFirst(request) {
 
 //Adding `activate` event listener
 self.addEventListener('activate', (event) => {
-  console.info('Event: Activate');
+    console.info('Event: Activate');
 
-  //Navigation preload is help us make parallel request while service worker is booting up.
-  //Enable - chrome://flags/#enable-service-worker-navigation-preload
-  //Support - Chrome 57 beta (behing the flag)
-  //More info - https://developers.google.com/web/updates/2017/02/navigation-preload#the-problem
+    //Navigation preload is help us make parallel request while service worker is booting up.
+    //Enable - chrome://flags/#enable-service-worker-navigation-preload
+    //Support - Chrome 57 beta (behing the flag)
+    //More info - https://developers.google.com/web/updates/2017/02/navigation-preload#the-problem
 
-  // Check if navigationPreload is supported or not
-  // if (self.registration.navigationPreload) { 
-  //   self.registration.navigationPreload.enable();
-  // }
-  // else if (!self.registration.navigationPreload) { 
-  //   console.info('Your browser does not support navigation preload.');
-  // }
+    // Check if navigationPreload is supported or not
+    // if (self.registration.navigationPreload) { 
+    //   self.registration.navigationPreload.enable();
+    // }
+    // else if (!self.registration.navigationPreload) { 
+    //   console.info('Your browser does not support navigation preload.');
+    // }
 
-  //Remove old and unwanted caches
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== cacheName) {
-            return caches.delete(cache); //Deleting the old cache (cache v1)
-          }
+    //Remove old and unwanted caches
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== cacheName) {
+                        return caches.delete(cache); //Deleting the old cache (cache v1)
+                    }
+                })
+            );
         })
-      );
-    })
-    .then(function () {
-      console.info("Old caches are cleared!");
-      // To tell the service worker to activate current one 
-      // instead of waiting for the old one to finish.
-      return self.clients.claim(); 
-    }) 
-  );
+        .then(function() {
+            console.info("Old caches are cleared!");
+            // To tell the service worker to activate current one 
+            // instead of waiting for the old one to finish.
+            return self.clients.claim();
+        })
+    );
 });
 
 /*
@@ -137,22 +136,22 @@ self.addEventListener('activate', (event) => {
 
 //Adding `push` event listener
 self.addEventListener('push', (event) => {
-  console.info('Event: Push');
+    console.info('Event: Push');
 
-  var title = 'Push notification demo';
-  var body = {
-    'body': 'click to return to application',
-    'tag': 'demo',
-    'icon': './images/icons/apple-touch-icon.png',
-    'badge': './images/icons/apple-touch-icon.png',
-    //Custom actions buttons
-    'actions': [
-      { 'action': 'yes', 'title': 'I ♥ this app!'},
-      { 'action': 'no', 'title': 'I don\'t like this app'}
-    ]
-  };
+    var title = 'Push notification demo';
+    var body = {
+        'body': 'click to return to application',
+        'tag': 'demo',
+        'icon': './images/icons/apple-touch-icon.png',
+        'badge': './images/icons/apple-touch-icon.png',
+        //Custom actions buttons
+        'actions': [
+            { 'action': 'yes', 'title': 'I ♥ this app!' },
+            { 'action': 'no', 'title': 'I don\'t like this app' }
+        ]
+    };
 
-  event.waitUntil(self.registration.showNotification(title, body));
+    event.waitUntil(self.registration.showNotification(title, body));
 });
 
 /*
@@ -162,22 +161,22 @@ self.addEventListener('push', (event) => {
 */
 
 self.addEventListener('sync', (event) => {
-  console.info('Event: Sync');
+    console.info('Event: Sync');
 
-  //Check registered sync name or emulated sync from devTools
-  if (event.tag === 'github' || event.tag === 'test-tag-from-devtools') {
-    event.waitUntil(
-      //To check all opened tabs and send postMessage to those tabs
-      self.clients.matchAll().then((all) => {
-        return all.map((client) => {
-          return client.postMessage('online'); //To make fetch request, check app.js - line no: 122
-        })
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-    );
-  }
+    //Check registered sync name or emulated sync from devTools
+    if (event.tag === 'github' || event.tag === 'test-tag-from-devtools') {
+        event.waitUntil(
+            //To check all opened tabs and send postMessage to those tabs
+            self.clients.matchAll().then((all) => {
+                return all.map((client) => {
+                    return client.postMessage('online'); //To make fetch request, check app.js - line no: 122
+                })
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+        );
+    }
 });
 
 /*
@@ -186,39 +185,38 @@ self.addEventListener('sync', (event) => {
 
 //Adding `notification` click event listener
 self.addEventListener('notificationclick', (event) => {
-  var url = 'https://demopwa.in/';
+    var url = 'https://demopwa.in/';
 
-  //Listen to custom action buttons in push notification
-  if (event.action === 'yes') {
-    console.log('I ♥ this app!');
-  }
-  else if (event.action === 'no') {
-    console.warn('I don\'t like this app');
-  }
+    //Listen to custom action buttons in push notification
+    if (event.action === 'yes') {
+        console.log('I ♥ this app!');
+    } else if (event.action === 'no') {
+        console.warn('I don\'t like this app');
+    }
 
-  event.notification.close(); //Close the notification
+    event.notification.close(); //Close the notification
 
-  //To open the app after clicking notification
-  event.waitUntil(
-    clients.matchAll({
-      type: 'window'
-    })
-    .then((clients) => {
-      for (var i = 0; i < clients.length; i++) {
-        var client = clients[i];
-        //If site is opened, focus to the site
-        if (client.url === url && 'focus' in client) {
-          return client.focus();
-        }
-      }
+    //To open the app after clicking notification
+    event.waitUntil(
+        clients.matchAll({
+            type: 'window'
+        })
+        .then((clients) => {
+            for (var i = 0; i < clients.length; i++) {
+                var client = clients[i];
+                //If site is opened, focus to the site
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
 
-      //If site is cannot be opened, open in new window
-      if (clients.openWindow) {
-        return clients.openWindow('/');
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    })
-  );
+            //If site is cannot be opened, open in new window
+            if (clients.openWindow) {
+                return clients.openWindow('/');
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+    );
 });
